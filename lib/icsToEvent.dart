899ce3dd64f581ecbae59ddb320fff12e45e4c77
7n14 <105433167+7n14@users.noreply.github.com>
2023-03-09 13:03:24 +0000

    Update icsToEvent.dart

--- lib/icsToEvent.dart
+++ lib/icsToEvent.dart
@@ -21,10 +21,25 @@ List icsToEvent(fileLocation) {
   final iCalData = iCalJSON['data'];
   for (final event in iCalData) {
     events.add(CleanCalendarEvent(event['summary'],
-        startTime: event['dtstart'],
-        endTime: event['dtend'],
+        startTime: parseDateString(event['dtstart']),
+        endTime: parseDateString(event['dtend']),
         color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
             .withOpacity(1.0)));
   }
   return events;
+}
+
+
+DateTime parseDateString(String input) {
+  RegExp regex = RegExp(
+      r'^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{3})Z$');
+  Match match = regex.firstMatch(input);
+  int year = int.parse(match.group(1));
+  int month = int.parse(match.group(2));
+  int day = int.parse(match.group(3));
+  int hour = int.parse(match.group(4));
+  int minute = int.parse(match.group(5));
+  int second = int.parse(match.group(6));
+  int millisecond = int.parse(match.group(7));
+  return DateTime.utc(year, month, day, hour, minute, second, millisecond);
 }
